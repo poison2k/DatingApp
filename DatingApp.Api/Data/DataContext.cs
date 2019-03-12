83d607198745cc333513b04372d5c82a6679bee9
Sebastian Bünck <buenck@gmx.de>
2019-03-12 19:08:03 +0100

    Add AuthRepository to Project and Add USer Class to DB

--- DatingApp.Api/Data/DataContext.cs
+++ DatingApp.Api/Data/DataContext.cs
@@ -8,6 +8,9 @@ namespace DatingApp.Api.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
 
         public DbSet<Value> Values {get; set;}
+
+        public DbSet<User> Users {get;set;}
+        
         
     }
 }
